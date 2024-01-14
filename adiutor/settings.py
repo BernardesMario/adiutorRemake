3213320641f96f9a02e72e6ab29bb1649d0b1c71
mario_bernardes <bernardesmario95@gmail.com>
2024-01-13 21:47:06 -0300

    Standardize env variables

--- adiutor/settings.py
+++ adiutor/settings.py
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adiutor.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('email_password', 'adiutorRemake')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'adiutor.noreply@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'adiutorRemake')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
