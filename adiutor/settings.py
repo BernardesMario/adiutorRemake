e203c5d42d050a95883fa0bf8e35f91e02c14aa8
mario_bernardes <bernardesmario95@gmail.com>
2024-02-01 16:03:20 -0300

    Adicionando Utils e Services

--- adiutor/settings.py
+++ adiutor/settings.py
@@ -136,7 +136,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'adiutor.noreply@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'adiutorRemake')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'vrcnlsutlezbbljm')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
