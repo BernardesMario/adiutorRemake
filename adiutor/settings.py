1a49619048c4912db6a577739c3159da2ad55dfb
mario_bernardes <bernardesmario95@gmail.com>
2024-04-08 15:59:56 -0300

    Update settings STATIC_ROOT

--- adiutor/settings.py
+++ adiutor/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/' if DEBUG else '/src/staticfiles/'
 
 # STATIC_URL = 'static/' if DEBUG else '/src/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
     STATIC_ROOT = '/src/staticfiles/'
 
